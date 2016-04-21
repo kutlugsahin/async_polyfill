f51782f130855a39ef33e084874167776a2d6d14
ahmetkutlu <ku>
2016-04-21 21:21:19 +0300

    init

---
+++ index.js
@@ -0,0 +1,22 @@
+module.exports = function (makeGenerator) {
+    return function () {
+        var generator = makeGenerator instanceof Function ? makeGenerator.apply(this, arguments) : makeGenerator;
+
+        function handle(result) {
+            // result => { done: [Boolean], value: [Object] }
+            if (result.done) return Promise.resolve(result.value);
+
+            return Promise.resolve(result.value).then(function (res) {
+                return handle(generator.next(res));
+            }, function (err) {
+                return handle(generator.throw(err));
+            });
+        }
+
+        try {
+            return handle(generator.next());
+        } catch (ex) {
+            return Promise.reject(ex);
+        }
+    }
+};
